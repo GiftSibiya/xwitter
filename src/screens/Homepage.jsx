85e4f065f0d36d17a21b15dcc88c1045966cce90
GiftSibiya <sibiya.bobo@gmail.com>
2024-01-13 18:50:56 +0200

    Tab View Implemented

--- src/screens/Homepage.jsx
+++ src/screens/Homepage.jsx
@@ -1,15 +1,41 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, useWindowDimensions } from "react-native";
 import React from "react";
+import { TabView, SceneMap } from "react-native-tab-view";
 
-const Homepage = ({ navigation }) => {
+const FirstRoute = () => {
+  <View>
+    {" "}
+    <Text> This is the first view </Text>{" "}
+  </View>;
+};
+const SecondRoute = () => {
+  <View>
+    {" "}
+    <Text> This is the first view </Text>{" "}
+  </View>;
+};
+
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
+});
+
+const Homepage = () => {
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: "first", title: "First" },
+    { key: "second", title: "Second" },
+  ]);
+
   return (
-    <View>
-      <Text>This is the homepage homepage And it works</Text>
-      <Button
-        title="Detail screen"
-        onPress={() => navigation.navigate("Details")}
-      ></Button>
-    </View>
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+    />
   );
 };
 
