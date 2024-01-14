e36b32101878d564e3d5d9fe9b24c3d481be885e
GiftSibiya <sibiya.bobo@gmail.com>
2024-01-14 07:15:59 +0200

    tab view

--- src/screens/Homepage.jsx
+++ src/screens/Homepage.jsx
@@ -1,29 +1,21 @@
 import { View, Text, Button, useWindowDimensions } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { TabView, SceneMap } from "react-native-tab-view";
 
-const FirstRoute = () => {
-  <View>
-    {" "}
-    <Text> This is the first view </Text>{" "}
-  </View>;
-};
-const SecondRoute = () => {
-  <View>
-    {" "}
-    <Text> This is the first view </Text>{" "}
-  </View>;
-};
+/// File imports ///
+import ForYou from "./tabView/ForYou";
+import Following from "./tabView/Following";
 
 const renderScene = SceneMap({
-  first: FirstRoute,
-  second: SecondRoute,
+  first: ForYou,
+  second: Following,
 });
 
 const Homepage = () => {
   const layout = useWindowDimensions();
 
-  const [index, setIndex] = React.useState(0);
+  const [index, setIndex] = useState(0);
+
   const [routes] = React.useState([
     { key: "first", title: "First" },
     { key: "second", title: "Second" },
---
+++ src/screens/tabView/ForYou.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const forYou = () => {
+  return (
+    <View>
+      <Text>forYou</Text>
+    </View>
+  );
+};
+
+export default forYou;
